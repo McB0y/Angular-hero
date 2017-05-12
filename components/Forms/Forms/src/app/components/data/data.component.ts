d8b7d08455ea37e926f5d5d14a5eeff7faeb3f60
McB0y <rafael.cisneros.gomez@gmail.com>
2017-05-12 01:04:45 -0500

    FormControl() I'm on my way

--- components/Forms/Forms/src/app/components/data/data.component.ts
+++ components/Forms/Forms/src/app/components/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// Los siguientes modulos deberan ser integrados "forzozamente".
+import { FormGroup, FormControl, Validators } form '@angular/forms'
 
 @Component({
   selector: 'app-data',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+    forma: FormGroup;// Esta variable hara one way binding dentro del template en la etiqueta form.
+
+  constructor() {
+
+   }
 
   ngOnInit() {
+    // I put this here 'cause it's so long & I wanna charge the component first
+    this.form = new FormGroup({
+        'name' : new FormControl('Rafael'),// you can pass opcional validations & async validations
+        'lastName' : new FormControl(), // I dont put any kinda defect value.
+        'correo' : new FormControl ()
+    })
   }
 
   //Starting with regular expressions and logic by this side.
